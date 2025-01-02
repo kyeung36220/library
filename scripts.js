a95ab5fb115dde4cc1b30091530ccc9ddd2b5fd3
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2025-01-02 17:45:45 -0500

    Adding general ui elements to cards

--- scripts.js
+++ scripts.js
@@ -1,23 +1,56 @@
-const body = document.querySelector(".body")
+const body = document.querySelector("#body")
+const button = document.querySelector("#addBookButton")
+const newBookDialog = document.querySelector(`#newBookDialog`)
 
+button.addEventListener("click", () => {
+    newBookDialog.showModal()
+})
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
-
-function addBookToLibrary() {
-  // do stuff here
-}
-
-const theHobbit = {
-    title: `The Hobbit`,
-    author: `J.R.R. Tolkien`,
-    pages: 295,
-    read: `not read yet`,
-    info() {
-        return`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+function Book(title, author, pages, completed) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.completed = completed;
+    this.toggleCompleteStatus = function() {
+        this.completed = this.completed === "Completed" ? "Not Completed" : "Completed"
     }
+
 }
 
-console.log(theHobbit.info());
+function addBookToLibrary(title, author, pages, completed) {
+  const book = new Book(title, author, pages, completed)
+  myLibrary.push(book)
+  updateListUI(book)
+}
+
+function updateListUI(book) {
+    let card = document.createElement("div")
+    card.className = "card"
+    buttonColor = book.completed === `Completed` ? `green` : `red`
+    card.innerHTML = `
+    <div class="entry">
+        <div class='title'>Title: ${book.title}</div>
+        <div class='author'> Author: ${book.author}</div>
+        <div class='pages'> Total Pages: ${book.pages}</div>
+        <div class='completed'>
+            <div style="display: flex; gap: 0.5vw; align-items: center; justify-content: center;">
+                <span class='toggleButton' type="button" style="background-color:${buttonColor}"></span>
+                <span class=completedText>${book.completed}</span>
+            </div>
+        </div>
+    </div>
+    `
+    let toggleButton = card.querySelector(".toggleButton")
+    let completedText = card.querySelector(".completedText")
+    toggleButton.addEventListener("click", () => {
+        book.toggleCompleteStatus()
+        toggleButton.style.backgroundColor = toggleButton.style.backgroundColor === `green` ? `red` : `green`
+        completedText.textContent = `${book.completed}`
+    })
+    body.appendChild(card)
+}
+
+addBookToLibrary(`Hobbit`, `John Smith`, `300`, "Completed")
+addBookToLibrary(`Gobbit`, `Sohn Smith`, `200`, "Not Completed")
+
