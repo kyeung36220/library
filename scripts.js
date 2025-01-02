d4f1ca31099ab8e99330ba90ff2f0451ebee408d
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2025-01-01 19:21:49 -0500

    Adding general ui

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const body = document.querySelector(".body")
+
 const myLibrary = [];
 
 function Book() {
@@ -7,3 +9,15 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+const theHobbit = {
+    title: `The Hobbit`,
+    author: `J.R.R. Tolkien`,
+    pages: 295,
+    read: `not read yet`,
+    info() {
+        return`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+}
+
+console.log(theHobbit.info());
